3614195fe9d81e61935e2dd423c5660020d1cf7f
Brittanie Pham <brittaniepham@gmail.com>
2018-09-06 15:55:08 -0600

    initial commit

---
+++ GpsPosition.java
@@ -0,0 +1,37 @@
+package gps;
+
+public class GpsPosition {
+	
+	private double latitude;
+	private double longitude;
+	private double elevation;
+	
+	public GpsPosition(double latitude, double longitude, double elevation) {
+		if (latitude < -90 || latitude > 90 || longitude < -180 || longitude > 180){
+			throw new IllegalArgumentException("Invalid latitude and/or longitude");
+		}
+//		else if ( longitude < 0.0 ) {
+//			throw new IllegalArgumentException("Invalid latitude and/or longitude");
+//		}
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.elevation = elevation;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public double getElevation() {
+		return elevation;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%.6f, %.6f (%.1f)", latitude, longitude, elevation);	
+	}
+}
